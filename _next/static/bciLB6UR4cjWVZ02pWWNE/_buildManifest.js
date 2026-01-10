caee4b3a033f9998467e93485961fda6d6eb19fc
FKN237 <fodjonelson22@gmail.com>
2026-01-10 19:09:12 +0400

    Updates

--- _next/static/bciLB6UR4cjWVZ02pWWNE/_buildManifest.js
+++ _next/static/bciLB6UR4cjWVZ02pWWNE/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Lumina-Jipange-Program-Website//_next/:path+",
+        "destination": "/Lumina-Jipange-Program-Website/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
